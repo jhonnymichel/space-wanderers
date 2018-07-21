6fa6072227adb8621c70867f373a47256cf673f1
Jhonny Michel <jhonnymichel@outlook.com>
2018-07-21 14:44:06 -0300

    responsiviness

--- Source/Game.hx
+++ Source/Game.hx
@@ -1,18 +1,43 @@
 package;
 
 import starling.display.Sprite;
+import starling.display.Quad;
+import openfl.events.Event;
 import starling.events.Event;
+import starling.utils.Color;
 
 class Game extends Sprite {
 
+  private var circle:Quad;
+  private var hud:Quad;
+
   public function new() {
     super();
-    this.addEventListener(Event.ADDED_TO_STAGE, this.onAddedToStage);
+    circle = new Quad(200, 200, Color.RED);
+    hud = new Quad(200, 200, Color.BLUE);
+    addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
   }
 
   private function onAddedToStage(e:Event):Void {
-    trace(stage.stageWidth);
-    trace(stage.stageHeight);
+    width = 1280;
+    height = 720;
+    addChild(circle);
+    circle.alignPivot();
+    circle.x = width / 2;
+    circle.y = height / 2;
+
+    hud.x = 0;
+    hud.y = stage.stageHeight;
+    hud.alignPivot('left',  'bottom');
+    stage.addChild(hud);
+  }
+
+  public function setupScreen(e:Event = null) {
+    x = (stage.stageWidth - width) / 2;
+    y = (stage.stageHeight - height) / 2;
+
+
+    hud.y = stage.stageHeight;
   }
 
 }
--- Source/Main.hx
+++ Source/Main.hx
@@ -1,32 +1,39 @@
 package;
 
 import openfl.display.Sprite;
+import starling.events.Event in StarlingEvent;
 import openfl.events.Event;
 import starling.core.Starling;
 import Math;
 
 class Main extends Sprite {
 
-	private var _starling:Starling;
+	private var starling:Starling;
+	private var game:Game;
 
 	public function new () {
-
 		super();
-		_starling  = new Starling(Game, stage);
-		_starling.showStats = true;
-		_starling.start();
-		this.setupScreen();
-		stage.addEventListener(Event.RESIZE, this.setupScreen);
+		starling  = new Starling(Game, stage);
+		starling.showStats = true;
+		starling.start();
+		starling.addEventListener(StarlingEvent.ROOT_CREATED, onStart);
+	}
+
+	private function onStart(e:Event, root:Game):Void {
+		game = root;
+		stage.addEventListener(Event.RESIZE, setupScreen);
+		setupScreen();
 	}
 
 	private function setupScreen(e:Event = null):Void {
-		_starling.viewPort.width = stage.stageWidth;
-		_starling.viewPort.height = stage.stageHeight;
-		var proportion:Float = _starling.viewPort.height / _starling.viewPort.width;
-		_starling.stage.stageWidth = 1280;
-		_starling.stage.stageHeight = Math.round(1280 * proportion);
-		trace(_starling.contentScaleFactor);
-		_starling.showStatsAt('left', 'top', 2 / _starling.contentScaleFactor);
+		var proportion:Float;
+		starling.viewPort.width = stage.stageWidth;
+	  starling.viewPort.height = stage.stageHeight;
+		proportion = starling.viewPort.width / starling.viewPort.height;
+		starling.stage.stageHeight = 720;
+		starling.stage.stageWidth = Math.round(720 * proportion);
+		starling.showStatsAt('left', 'top', 2 / starling.contentScaleFactor);
+		game.setupScreen();
 	}
 
 
