d8c1a900223b5c962d9e4d40274587f8df692d2d
Jhonny Michel <jhonnymichel@evolux.net.br>
2018-12-19 20:41:23 -0300

    Simplifying chase logic

--- Source/objects/characters/Enemy.hx
+++ Source/objects/characters/Enemy.hx
@@ -16,6 +16,8 @@ class Enemy extends Spaceship {
   public function new(target:ICameraTarget) {
     super();
     this.target = target;
+    INERTIA_RATE = Math.pow(2, 6);
+    TURN_RATE *= 1;
     movement[Keyboard.UP] = true;
     x = 500;
     y = 500;
@@ -24,47 +26,17 @@ class Enemy extends Spaceship {
     alignPivot();
   }
 
-  private function getPointOfAvoidance(boundaries:Rectangle):Point {
-    if (collisionAvoidanceDirection == null) {
-      collisionAvoidanceDirection = Std.random(3);
-    }
-
-    return switch(collisionAvoidanceDirection) {
-      case 0:
-        new Point(boundaries.x + 900, boundaries.y);
-      case 1:
-        new Point(boundaries.x - 900, boundaries.y);
-      case 2:
-        new Point(boundaries.x, boundaries.y + 900);
-      case 3:
-        new Point(boundaries.x, boundaries.y - 900);
-      default:
-        new Point(boundaries.x, boundaries.y);
-    }
-  }
-
   override public function update():Void {
     var deltaX:Float = target.getBoundaries().x - x;
     var deltaY:Float = target.getBoundaries().y - y;
-    var angleToLook:Float = Math.atan2(deltaY, deltaX);
-    var distance:Float = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
-    if (isAvoidingCollision) {
-      deltaX = getPointOfAvoidance(target.getBoundaries()).x;
-      deltaY = getPointOfAvoidance(target.getBoundaries()).y;
-      alpha = .3;
-      angleToLook = Math.atan2(deltaY, deltaX);
-    } else if (distance < 250) {
-      isAvoidingCollision = true;
+    var angleToLook:Float = Math.atan2(deltaY, deltaX) + INITIAL_ROTATION;
+    var absoluteRotation = rotation < 0 ? rotation + Math.PI * 2 : rotation;
+
+    if (Math.max(angleToLook, absoluteRotation) - Math.min(angleToLook, absoluteRotation) > 1) {
+      rotation = angleToLook;
     } else {
-      alpha = 1;
+      rotation = absoluteRotation + (angleToLook - absoluteRotation) / 16;
     }
-
-    if (distance > 600) {
-      isAvoidingCollision = false;
-      collisionAvoidanceDirection = null;
-    }
-
-    rotation = angleToLook + INITIAL_ROTATION;
 
     super.update();
   }
